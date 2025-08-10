b2b1a65adb7c85bf9d8a6e288a5b93cb57695926
Zotov Slava <zotovvslava1@mail.ru>
2025-08-10 18:21:39 +0300

    Спринт 21 V1

--- telemetry/collector/src/main/java/ru/yandex/practicum/collector/MainServer.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/collector/MainServer.java
@@ -2,8 +2,10 @@ package ru.yandex.practicum.collector;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class MainServer {
     public static void main(String[] args) {
         SpringApplication.run(MainServer.class, args);
